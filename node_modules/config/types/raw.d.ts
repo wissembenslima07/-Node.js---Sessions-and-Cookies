8d85d00a0f42a67c21c42897f58a208cdebe6965
wissem ben slima <wissem.ben.slima@tek-up.de>
2026-04-03 23:58:16 +0100

    gitignore

--- node_modules/config/types/raw.d.ts
+++
@@ -1,9 +0,0 @@
-import { RawConfig } from "./lib/util";
-/**
- * @param {any} rawObj
- * @returns {RawConfig & { resolve: () => any }}
- */
-export function raw(rawObj: any): RawConfig & {
-    resolve: () => any;
-};
-export { RawConfig };
